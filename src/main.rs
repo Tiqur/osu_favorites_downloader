b35a3b62e8827540cc533291d09374a907c8119c
Tiqur <trevorbrage02@gmail.com>
2023-05-10 15:59:09 -0700

    Added dotenv crate

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
 use reqwest;
+use dotenv::dotenv;
+use std::env;
 
 fn main() {
+    // Load env variables
+    dotenv().ok();
+    let client_id = env::var("CLIENT_ID").expect("CLIENT_ID not set");
+    let client_secret = env::var("CLIENT_SECRET").expect("CLIENT_SECRET not set");
+
+    // Send request
     let res = reqwest::blocking::get("http://httpbin.org/get").unwrap();
     println!("{}", res.status());
 }
