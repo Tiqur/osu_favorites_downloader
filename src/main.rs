8e33e95c43c5f3038c3ecccea78b0b20b7c5180e
Tiqur <trevorbrage02@gmail.com>
2023-05-11 12:41:51 -0700

    Return access_token string instead of json

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,14 @@
 use reqwest::Url;
 use reqwest::blocking::Client;
 use dotenv::dotenv;
-use reqwest::header::{HeaderMap, HeaderValue, ACCEPT, CONTENT_TYPE};
+use reqwest::header::{HeaderMap, HeaderValue, ACCEPT, CONTENT_TYPE, AUTHORIZATION};
 use std::env;
 
 const API_ENDPOINT: &str = "https://osu.ppy.sh/api/v2";
 
 
-fn get_access_token() -> Result<String, reqwest::Error> {
-    let client_id = env::var("CLIENT_ID").expect("CLIENT_ID not set");
-    let client_secret = env::var("CLIENT_SECRET").expect("CLIENT_SECRET not set");
-    
-    // Send request
+fn fetch_access_token(client_id: String, client_secret: String) -> String {
+    // Create URL
     let url = Url::parse("https://osu.ppy.sh/oauth/token").expect("Something went wrong parsing URL");
 
     // Create header map
@@ -26,15 +23,45 @@ fn get_access_token() -> Result<String, reqwest::Error> {
         .body(format!("client_id={}&client_secret={}&grant_type=client_credentials&scope=public", client_id, client_secret))
         .send().expect("Something went wrong sending request");
 
-    res.text()
+    let parsed_json: serde_json::Value = serde_json::from_str(res.text().unwrap().as_str()).unwrap();
+    
+    parsed_json["access_token"].as_str().expect("Something went wrong parsing Bearer token").to_string()
+}
+
+fn fetch_favourite_beatmaps(api_endpoint_url: &Url, token: &String, user_id: i32) {
+    let client = Client::new();
+
+    // Create header map
+    let mut header_map = HeaderMap::new();
+    header_map.insert(ACCEPT, HeaderValue::from_static("application/json"));
+    header_map.insert(CONTENT_TYPE, HeaderValue::from_static("application/json"));
+    header_map.insert(AUTHORIZATION, HeaderValue::from_str(("Bearer ".to_owned() + token).as_str()).unwrap());
+
+    println!("{}", ("Bearer ".to_owned() + token).as_str());
+
+    // Send request
+    let res = client.get(api_endpoint_url.clone())
+        .headers(header_map)
+        .send().expect("Something went wrong sending request");
+
+    println!("{}", res.status());
 }
 
 fn main() {
     // Load env variables
     dotenv().ok();
+
+    // Set variables from env 
+    let client_id = env::var("CLIENT_ID").expect("CLIENT_ID not set");
+    let client_secret = env::var("CLIENT_SECRET").expect("CLIENT_SECRET not set");
+    
+    // Create URL
+    let api_endpoint_url = Url::parse(API_ENDPOINT).expect("Something went wrong parsing URL");
     
     // Obtain access token using OAuth
-    let access_token = get_access_token();
+    let access_token = fetch_access_token(client_id, client_secret);
+    println!("{}", access_token);
 
-    println!("{}", access_token.unwrap());
+    // Get favorited beatmaps
+    //fetch_favourite_beatmaps(&api_endpoint_url, &access_token, 1093872);
 }
