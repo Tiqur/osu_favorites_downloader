ed96b4b7cf0c585b0311bd45d34f0e9377f5f5b6
Tiqur <trevorbrage02@gmail.com>
2023-05-11 12:56:11 -0700

    Move url creation into function

--- src/main.rs
+++ src/main.rs
@@ -28,7 +28,7 @@ fn fetch_access_token(client_id: String, client_secret: String) -> String {
     parsed_json["access_token"].as_str().expect("Something went wrong parsing Bearer token").to_string()
 }
 
-fn fetch_favourite_beatmaps(api_endpoint_url: &Url, token: &String, user_id: i32) {
+fn fetch_favourite_beatmaps(token: &String, user_id: i32) {
     let client = Client::new();
 
     // Create header map
@@ -37,10 +37,11 @@ fn fetch_favourite_beatmaps(api_endpoint_url: &Url, token: &String, user_id: i32
     header_map.insert(CONTENT_TYPE, HeaderValue::from_static("application/json"));
     header_map.insert(AUTHORIZATION, HeaderValue::from_str(("Bearer ".to_owned() + token).as_str()).unwrap());
 
-    println!("{}", ("Bearer ".to_owned() + token).as_str());
-
+    // Create URL
+    let api_endpoint_url = Url::parse((API_ENDPOINT.to_owned() + format!("/users/{}/beatmapsets/loved", user_id.to_string()).as_str()).as_str()).expect("Something went wrong parsing URL");
+    
     // Send request
-    let res = client.get(api_endpoint_url.clone())
+    let res = client.get(api_endpoint_url)
         .headers(header_map)
         .send().expect("Something went wrong sending request");
 
@@ -55,13 +56,9 @@ fn main() {
     let client_id = env::var("CLIENT_ID").expect("CLIENT_ID not set");
     let client_secret = env::var("CLIENT_SECRET").expect("CLIENT_SECRET not set");
     
-    // Create URL
-    let api_endpoint_url = Url::parse(API_ENDPOINT).expect("Something went wrong parsing URL");
-    
     // Obtain access token using OAuth
     let access_token = fetch_access_token(client_id, client_secret);
-    println!("{}", access_token);
 
     // Get favorited beatmaps
-    //fetch_favourite_beatmaps(&api_endpoint_url, &access_token, 1093872);
+    fetch_favourite_beatmaps(&access_token, 14852499);
 }
